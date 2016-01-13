33c7f59f0e98bbdab640320df9f144f0363530e8
schmuhl <schmuhl@gmail.com>
2016-01-13 16:52:02 -0700

    Layout updates; added statistics (average, etc) and a configuration file

--- index.php
+++ index.php
@@ -14,12 +14,15 @@ $metrics = Metric::getAllMetrics();
 <h1>Metrics</h1>
 <table>
 	<tr>
-		<th colspan="2">Metric</th>
-		<th>Value</th>
-		<th>Since</th>
+      <th colspan="2">Metric</th>
+      <th>Value</th>
+      <th>Last Recorded</th>
+      <th>Average</th>
+      <th>MOE</th>
 	</tr>
 	<?php foreach ( $metrics as $metric ) { 
 		$recording = $metric->getLastRecording();
+        $metric->getRecordings(100);
 		$recorded = @strtotime($recording->recorded);
 		if ( $recorded > time()-(60*60*23) ) {
 		    $status = "current";
@@ -34,12 +37,27 @@ $metrics = Metric::getAllMetrics();
 		    $status = "error";
 		    @$statusDescription = "This metrics is no longer being recorded - $recording->recorded";
 		}
+
+       // Calculate the statistics on this set of numbers
+       $total = 0;
+       $count = 0;
+       $values = array();
+       foreach ( $metric->recordings as $recording ) {
+          $total += $recording->value;
+          $count ++;
+          $values []= $recording->value;
+       }
+       $average = $total/$count;
+       $stdDev = standard_deviation($values);
+       $moe = margin_of_error($stdDev,$count);
 		?>
 	<tr>
 	    <td><img src="images/<?php echo $status; ?>.png" alt="<?php echo $statusDescription; ?>" title="<?php echo $statusDescription; ?>" /></td>
 		<td><a href="metric.php?metric=<?php echo $metric->metricID; ?>"><?php echo $metric->name; ?></a></td>
 		<td align="right"><?php if ( $recording instanceOf MetricRecording ) echo $metric->value($recording->value); ?></td>
-		<td><?php if ( $recording instanceOf MetricRecording ) echo $metric->toDate($recording->recorded,true); ?></td>
+       <td><?php if ( $recording instanceOf MetricRecording ) echo $metric->toDate($recording->recorded,true); ?></td>
+       <td><?php if ( $recording instanceOf MetricRecording ) echo $metric->value($average); ?></td>
+       <td><?php if ( $recording instanceOf MetricRecording ) echo '&plusmn;'.$metric->value($moe,true); ?></td>
 	</tr>
 	<?php } ?>
 </table>
