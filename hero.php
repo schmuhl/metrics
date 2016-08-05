7129fa4bca7c3db589685d0781254b9038673652
schmuhl <schmuhl@gmail.com>
2016-08-05 17:25:32 -0600

    Refinements and tweaks to the hero page

--- hero.php
+++ hero.php
@@ -15,8 +15,6 @@ require 'metric-functions.inc';
 
 // Grab since the last Fall semester
 $metric->getRecordings(null,'daily',"2015-07-01","now");
-$value = number_format($metric->recordings[0]->value);
-$caption = date("F jS",strtotime($metric->recordings[0]->recorded));
 
 // get this year's and the last year comparison
 $r1 = array();
@@ -26,7 +24,16 @@ foreach ( $metric->recordings as $recording ) {
   if ( $recording->recorded >= '2015-07-15' && $recording->recorded < '2016-01-01' ) $r1[]= $recording;
   else if ( $recording->recorded >= '2016-07-15' ) $r2[]= $recording;
 }
-if ( count($r2) == 0 ) unset($r2);
+if ( count($r2) == 0 ) {  // what? nothing in this year?
+    $value = number_format($r1[count($r1)-1]->value);
+    $caption = date("F jS",strtotime($r1[count($r1)-1]->recorded));
+  unset($r2);
+} else {  // this is the expected case
+    $value = number_format($r2[count($r2)-1]->value);
+    $caption = date("F jS",strtotime($r2[count($r2)-1]->recorded));
+}
+
+
 
 
 if ( $showHeading ) showHeader($metric->name);
@@ -39,11 +46,11 @@ if ( $showHeading ) showHeader($metric->name);
   DIV#body DIV.content { width: 100%; }
 
   DIV.hero { width: 100%; display: block; overflow: hidden; }
-  DIV.hero H1 { font-size: 100px; text-align: center; }
+  DIV.hero H1 { margin: 0 0 20px 0; padding: 0; font-size: 80px; text-align: center; line-height: 120px; }
   DIV.hero DIV.today { float: left; margin: 50px 0; border: 1px solid gray; width: 460px; height: 400px; text-align: center; }
   DIV.hero DIV.today DIV.value { height: 300px; font-size: 170px; line-height: 300px; }
   DIV.hero DIV.today DIV.caption { hight: 100px; background: gray; color: white; font-size: 60px; line-height: 100px; }
-  DIV.hero DIV#chart_div { float: right; display: block; margin: 30px auto 0 auto; width: 800px !important; height: 400px !important; overflow: hidden; }
+  DIV.hero DIV#chart_div { float: right; margin: -10px 0 0 0 !important; padding: 0 !important; width: 800px !important; height: 600px !important; over1flow: hidden; }
 </style>
 
 
@@ -68,7 +75,7 @@ if ( $showHeading ) showHeader($metric->name);
   google.setOnLoadCallback(drawChart);
   function drawChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Date', 'This Year' <?php if ( isset($r2) ) echo ", 'Last Year'"; ?> ],
+    ['Date', 'Last Year' <?php if ( isset($r2) ) echo ", 'This Year'"; ?> ],
         <?php $max = isset($r2) ? max(count($r2),count($r1)) : count($r1); // how many data points to show ?>
     <?php for ( $i = 0; $i < $max; $i++ ) { ?>
     ['<?php echo date("j-M",strtotime($r1[$i]->recorded)); ?>',  <?php echo $metric->value($r1[$i]->value,true); ?> <?php if ( isset($r2) ) echo ", ".$metric->value($r2[$i]->value,true); ?>],
@@ -77,7 +84,8 @@ if ( $showHeading ) showHeader($metric->name);
 
   var options = {
     title: '<?php echo $metric->name; ?>',
-        <?php if ( !isset($recordings2) ) { ?>'legend':'none',<?php } ?>
+    <?php if ( !isset($r2) ) { ?>'legend':'none',<?php } ?>
+    height: 500,
     animation: {
       duration: 1000,
       easing: 'out'
